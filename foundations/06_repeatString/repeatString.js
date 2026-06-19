102eb20cc84f3eb9080f32f4e08b92eb03c0c178
Euphorxx <97964078+Euphorxx@users.noreply.github.com>
2026-06-19 11:46:45 +0000

    solve ex-06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+if (typeof(string) !== 'string' || num < 0) {return 'ERROR';
+}
+else {
+    let arr = []
+    for (let i = 0 ; i < num ; i++) {
+        arr.push(string);
+    }
+    return arr.join('');
+}
 
 };
+
+console.log(repeatString('hey', 3)) // returns 'heyheyhey'
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
