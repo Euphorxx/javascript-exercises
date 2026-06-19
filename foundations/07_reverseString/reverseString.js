d9e92a37bf556ae151601e157e6e68ae396dca60
Euphorxx <97964078+Euphorxx@users.noreply.github.com>
2026-06-19 12:16:57 +0000

    solve ex-07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+arr = string.split('');
+arr.reverse();
+return arr.join('');
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
