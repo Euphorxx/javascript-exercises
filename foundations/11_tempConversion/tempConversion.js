e2790c4dab0745de2b31e8a247c35d5f14c30c50
Euphorxx <97964078+Euphorxx@users.noreply.github.com>
2026-06-23 16:04:48 +0000

    solve ex-11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+const fahrenheitToCelsius = 5 / 9 * (tempInFahrenheit - 32)
+return Math.round(fahrenheitToCelsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+const celsiusToFahrenheit = (9 / 5 * tempInCelsius) + 32
+return Math.round(celsiusToFahrenheit * 10) / 10
 };
 
 // Do not edit below this line
