9baf3c69421bf8087f314908b8df34ef7657683b
Euphorxx <97964078+Euphorxx@users.noreply.github.com>
2026-06-23 14:47:48 +0000

    solve ex-08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr,...args) { 
+args.forEach((item) => {
+    while(arr.includes(item)) {
+        let index = arr.indexOf(item)
+        arr.splice(index,1)
+    }
+})
+return arr;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
