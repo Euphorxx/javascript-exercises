5200367e369937e0d4498f80d7f639267a1079ad
Euphorxx <97964078+Euphorxx@users.noreply.github.com>
2026-06-23 15:31:16 +0000

    solve ex-10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+if (num % 4 === 0 && num % 100 !== 0 ) return true;
+else if (num % 100 === 0 && num % 400 === 0) return true;
+else return false
 };
+
+console.log(leapYears(2024));
 
 // Do not edit below this line
 module.exports = leapYears;
