9e4bf80c7f4681116bd22a574b976f2b36391e0c
Euphorxx <97964078+Euphorxx@users.noreply.github.com>
2026-06-23 14:48:05 +0000

    solve ex-09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+let nums = [];
+let num1;
+let num2;
+if(!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+else if (a < 0 || b < 0) return 'ERROR';
 
+if (a > b) num1 = b, num2 = a;
+else if (b > a) num1 = a, num2 = b;
+
+for(let i = num1 ; i <= num2 ; i++) {
+nums.push(i);
+}
+return nums.reduce((acc,curr) => acc + curr)
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
